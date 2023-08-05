1910dd12e0b76343bff0a9f69f698f146359264d
MiroslavNaumovski <sokobanjafox@gmail.com>
2023-08-05 10:10:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kako zivis bez okej CUKANEEEEE
         </p>
         <a
           className="App-link"
